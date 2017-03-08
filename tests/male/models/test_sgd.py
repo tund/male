81d6ff25333511c862ec916eaa29ad0614cb0874
Khanh Dang Kim Nguyen <khanhndk@hcmup.edu.vn>
2017-03-08 15:59:20 +1100

    Add visualization utilities for 2D data, tested with KSGD model

--- tests/male/models/test_sgd.py
+++ tests/male/models/test_sgd.py
@@ -15,8 +15,49 @@ from sklearn.model_selection import PredefinedSplit
 from sklearn.model_selection import StratifiedShuffleSplit
 
 from male.models.kernel import KSGD
+from male.callbacks import Display
 
 data_dir = 'C:/Data/'
+
+
+def test_sgd_visualization_2d():
+    sub_folder = '2d/'
+    data_name = 'train.scale'
+    n_features = 2
+    train_file_name = data_dir + sub_folder + data_name + '.txt'
+
+    x_train, y_train = load_svmlight_file(train_file_name, n_features=n_features)
+
+    x_train = x_train.toarray()
+
+    display = Display(dpi='auto', freq=10,
+                      monitor=[{'metrics': ['predict'],
+                                'title': "Prediction",
+                                'xlabel': "X1",
+                                'ylabel': "X2",
+                                'grid_size': 100,
+                                'marker_size': 10,
+                                'left': None,
+                                'right': None,
+                                'top': None,
+                                'bottom': None
+                                }]
+                      )
+
+    learner = KSGD(lbd=0.0001,
+                   eps=0.001,
+                   gamma=30,
+                   kernel='gaussian',
+                   loss='hinge',
+                   batch_size=1,
+                   callbacks=[display],
+                   avg_weight=False)
+
+    learner.fit(x_train, y_train)
+
+    y_train_pred = learner.predict(x_train)
+
+    print("Training error = %.4f" % (1 - metrics.accuracy_score(y_train, y_train_pred)))
 
 
 def test_sgd_svmguide1_bin():
@@ -49,5 +90,6 @@ def test_sgd_svmguide1_bin():
 
 
 if __name__ == '__main__':
-    # pytest.main([__file__])
-    test_sgd_svmguide1_bin()
+    pytest.main([__file__])
+    # test_sgd_svmguide1_bin()
+    # test_sgd_visualization_2d()
