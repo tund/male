4067e438fd7e837b6c10a754480a8f96841540e4
khanhndk <khanhndk@hcmup.edu.vn>
2017-03-19 13:13:01 +1100

    rename variable

--- male/models/distribution/gaussian_invwishart.py
+++ male/models/distribution/gaussian_invwishart.py
@@ -10,32 +10,32 @@ class GaussianInvWishart(object):
     """Gaussian Inverse Wishart distribution
     Prioritize speed
     """
-    def __init__(self, d, mu, lbd, nu, psi_m):
+    def __init__(self, dim, loc, lbd, degree, scale_mat):
         """
         Complete Initialize
-        :param d: dimension
-        :param mu: (d,) or scalar
+        :param dim: dimension of data
+        :param loc: location of data (d,) or scalar
         :param lbd: scalar
-        :param nu: scalar > d - 1
-        :param psi_m: (d,d)
+        :param degree: degree of freedom, scalar > d - 1
+        :param scale_mat: (d,d)
         """
-        self.d = d
-        if nu <= d - 1:
-            raise ValueError("nu must be greater than d - 1")
-        if np.isscalar(mu):
-            mu *= np.ones(d)
+        self.dim = dim
+        if degree <= dim - 1:
+            raise ValueError("degree must be greater than dim - 1")
+        if np.isscalar(loc):
+            loc *= np.ones(dim)
 
-        self.mu0 = mu.copy()
+        self.loc0 = loc.copy()
         self.lbd0 = lbd
-        self.nu0 = nu
-        self.psi0_m = psi_m.copy()
+        self.degree0 = degree
+        self.scale_mat = scale_mat.copy()
 
-        self.n = 0
-        self.x_sum = np.zeros(d)
-        self.mu = self.mu0
+        self.num_samples = 0
+        self.x_sum = np.zeros(dim)
+        self.loc = self.loc0
         self.lbd = self.lbd0
-        self.nu = self.nu0
-        self.psi_m = self.psi0_m
+        self.degree = self.degree0
+        self.scale_mat = self.scale_mat
         self.x_lst = []
         self.invalid = False
 
@@ -46,8 +46,8 @@ class GaussianInvWishart(object):
         """
         if self.invalid:
             self.update()
-        cov = stats.invwishart.rvs(df=self.nu, scale=self.psi_m)
-        mean = stats.multivariate_normal.rvs(mean=self.mu, cov=cov)
+        cov = stats.invwishart.rvs(df=self.degree, scale=self.scale_mat)
+        mean = stats.multivariate_normal.rvs(mean=self.loc, cov=cov / self.lbd)
         return mean, cov
 
     def update(self):
@@ -55,16 +55,16 @@ class GaussianInvWishart(object):
         Up to date parameters after add or delete item
         :return:
         """
-        if self.n > 0:
-            x_bar = self.x_sum / self.n
+        if self.num_samples > 0:
+            x_bar = self.x_sum / self.num_samples
             dx_bar = self.x_lst - x_bar  # array (n,d)
             C = np.dot(dx_bar.T, dx_bar)  # array (d,d)
         else:
-            x_bar = np.zeros(self.d)
-            C = np.zeros((self.d, self.d))
-        x_bar_mu = (x_bar - self.mu0).reshape((self.d,1))
-        self.mu = (self.lbd0 * self.mu0 + self.x_sum) / self.lbd
-        self.psi_m = self.psi0_m + C + (self.lbd0 * self.n) * np.dot(x_bar_mu, x_bar_mu.T) / self.lbd
+            x_bar = np.zeros(self.dim)
+            C = np.zeros((self.dim, self.dim))
+        x_bar_mu = (x_bar - self.loc0).reshape((self.dim, 1))
+        self.loc = (self.lbd0 * self.loc0 + self.x_sum) / self.lbd
+        self.scale_mat = self.scale_mat + C + (self.lbd0 * self.num_samples) * np.dot(x_bar_mu, x_bar_mu.T) / self.lbd
 
     def add_item(self, x):
         """
@@ -72,13 +72,13 @@ class GaussianInvWishart(object):
         :param x: (d,)
         :return: index of item in the component
         """
-        self.n += 1
+        self.num_samples += 1
         self.lbd += 1
-        self.nu += 1
+        self.degree += 1
         self.x_sum += x
         self.x_lst.append(x)
         self.invalid = True
-        return self.n - 1  # zero-based index
+        return self.num_samples - 1  # zero-based index
 
     def del_item(self, index):
         """
@@ -86,10 +86,9 @@ class GaussianInvWishart(object):
         :param index: (d,)
         :return:
         """
-        self.n -= 1
+        self.num_samples -= 1
         self.lbd -= 1
-        self.nu -= 1
+        self.degree -= 1
         self.x_sum -= self.x_lst[index]
         del self.x_lst[index]
         self.invalid = True
-
