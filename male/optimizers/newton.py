9df716c0cfad056991759edb1e28b4c5cb3d09b6
khanhndk <khanhndk@hcmup.edu.vn>
2017-03-28 10:14:09 +1100

    change x to vars

--- male/optimizers/newton.py
+++ male/optimizers/newton.py
@@ -25,20 +25,20 @@ class Newton(Optimizer):
     def update_params(self, *args, **kwargs):
         raise NotImplementedError
 
-    def solve(self, x=None, *args, **kwargs):
+    def solve(self, vars=None, *args, **kwargs):
         obj_lst = []
         for l in range(self.max_loop):
-            grad = self.grad_func(x, *args)
-            hess = self.hess_func(x, *args)
+            grad = self.grad_func(vars, *args)
+            hess = self.hess_func(vars, *args)
             hess_inv = np.linalg.inv(hess)
-            d = -np.dot(hess_inv, grad.reshape((len(x), 1)))
-            d = d.reshape(len(x))
+            d = -np.dot(hess_inv, grad.reshape((len(vars), 1)))
+            d = d.reshape(len(vars))
             t = 1
-            obj_x = self.obj_func(x, *args)
-            while self.obj_func(x + t * d, *args) > obj_x + 0.5 * t * np.dot(grad, d):
+            obj_x = self.obj_func(vars, *args)
+            while self.obj_func(vars + t * d, *args) > obj_x + 0.5 * t * np.dot(grad, d):
                 t = self.learning_rate * t
-            x += t * d
-            obj_lst.append(self.obj_func(x, *args))
+            vars += t * d
+            obj_lst.append(self.obj_func(vars, *args))
             if (l > 1) and np.abs((obj_lst[l] - obj_lst[l - 1]) / obj_lst[l]) < self.tolerance:
                 break
-        return x, obj_lst[-1]
+        return vars, obj_lst[-1]
