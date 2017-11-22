0862c36995957a6cea5f898ea6bd2d38e9675d66
Tu Dinh Nguyen <nguyendinhtu@gmail.com>
2017-11-23 10:38:22 +1100

    Add Conditional Generative Adversarial Nets (CGAN)
    * Add Conditional Generative Adversarial Nets (CGAN)
    
    * Fix typos
    
    * Fix loading CIFAR10 datasets
    
    * Reduce layer sizes and number of epochs

--- male/models/deep_learning/generative/__init__.py
+++ male/models/deep_learning/generative/__init__.py
@@ -5,8 +5,12 @@ try:
     from .dcgan import DCGAN
     from .gan1d import GAN1D
     from .gan2d import GAN2D
+    from .cgan import CGAN
+    from .cgan import CGANv1
 except ImportError:
     GAN = None
     DCGAN = None
     GAN1D = None
     GAN2D = None
+    CGAN = None
+    CGANv1 = None
