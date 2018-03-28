f4e9d1f82b32868362f654fa67f428fce4bedaf8
Tu Dinh Nguyen <nguyendinhtu@gmail.com>
2018-03-28 21:12:41 +1100

    Change test scripts for GAN1D and GAN2D

--- tests/male/models/deep_learning/generative/test_gan1d.py
+++ tests/male/models/deep_learning/generative/test_gan1d.py
@@ -2,9 +2,11 @@ from __future__ import division
 from __future__ import print_function
 from __future__ import absolute_import
 
+import os
 import sys
 import pytest
 
+from male.configs import model_dir
 from male.configs import random_seed
 from male.callbacks import Display
 from male.models.distribution import GMM1D
@@ -43,6 +45,8 @@ def test_gan1d_gaussian1d(show_figure=False, block_figure_on_end=False):
                                    title='Histogram',
                                    show=show_figure,
                                    block_on_end=block_figure_on_end,
+                                   # filepath=[os.path.join(model_dir(),
+                                   #                        "GAN1D/samples/hist_{epoch:04d}.png")],
                                    monitor=[{'metrics': ['distribution'],
                                              'type': 'hist',
                                              'title': "Histogram of GAN1D",
@@ -66,10 +70,10 @@ def test_gan1d_gaussian1d(show_figure=False, block_figure_on_end=False):
 
     model = GAN1D(data=Gaussian1D(mu=4.0, sigma=0.5),
                   generator=Uniform1D(low=-8.0, high=8.0),
-                  num_z=10,  # increase this to 100 for a full run
-                  num_epochs=4,  # increase this to 1000 for a full run
-                  hidden_size=16,  # increase this to 128 for a full run
-                  batch_size=128,
+                  num_z=8,  # set to 100 for a full run
+                  num_epochs=4,  # set to 355 for a full run
+                  hidden_size=4,  # set to 128 for a full run
+                  batch_size=8,  # set to 128 for a full run
                   minibatch_discriminator=False,
                   loglik_freq=1,
                   generator_learning_rate=0.0001,
@@ -134,10 +138,10 @@ def test_gan1d_gmm1d(show_figure=False, block_figure_on_end=False):
 
     model = GAN1D(data=GMM1D(pi=[0.55, 0.45], mu=[1.0, 4.0], sigma=[0.2, 0.5]),
                   generator=Uniform1D(low=-8.0, high=8.0),
-                  num_z=100,  # increase this to 100 for a full run
-                  num_epochs=4,  # increase this to 1000 for a full run
-                  hidden_size=16,  # increase this to 128 for a full run
-                  batch_size=128,
+                  num_z=8,  # set to 100 for a full run
+                  num_epochs=4,  # set to 1000 for a full run
+                  hidden_size=4,  # set to 128 for a full run
+                  batch_size=8,  # set to 128 for a full run
                   loglik_freq=1,
                   generator_learning_rate=0.0001,
                   discriminator_learning_rate=0.0001,
