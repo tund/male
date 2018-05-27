c7480d507a94a0fe439d06508da94c0382887e76
Tu Dinh Nguyen <nguyendinhtu@gmail.com>
2018-05-27 21:18:01 +1000

    Add RMSProp optimizer to TensorFlow backend

---
+++ male/models/distribution/__init__.py
@@ -0,0 +1,7 @@
+from .mixture import GMM
+from .mixture import GMM1D
+from .uniform import Uniform
+from .uniform import Uniform1D
+from .gaussian import Gaussian
+from .gaussian import Gaussian1D
+from .gaussian import InverseGaussian1D
---
+++ male/models/distribution/gaussian.py
@@ -0,0 +1,60 @@
+from __future__ import division
+from __future__ import print_function
+from __future__ import absolute_import
+
+import numpy as np
+from scipy.stats import norm
+from scipy.stats import multivariate_normal
+
+from ...configs import epsilon
+
+
+class Gaussian1D(object):
+    def __init__(self, mu=0.0, sigma=1.0):
+        self.mu = mu
+        self.sigma = sigma
+
+    def sample(self, num_samples):
+        samples = np.random.normal(self.mu, self.sigma, num_samples)
+        return samples
+
+    def logpdf(self, samples):
+        return np.mean(np.log(norm.pdf(samples, loc=self.mu, scale=self.sigma) + epsilon()))
+
+
+class InverseGaussian1D(object):
+    def __init__(self, mu=0.0, sigma=1.0, low=-6, high=6):
+        self.mu = mu
+        self.sigma = sigma
+        self.low = low
+        self.high = high
+
+    def sample(self, num_samples, num_bins=10):
+        s = np.random.normal(self.mu, self.sigma, num_samples)
+        bins = np.linspace(self.low, self.high, num_bins)
+        pd, _ = np.histogram(s, bins=bins, density=True)
+        pd /= np.sum(pd)
+        pd = 1 - pd
+        pd /= np.sum(pd)
+        z = np.random.multinomial(num_samples, pd)
+        samples = []
+        for i in range(len(z)):
+            samples.extend(bins[i] + np.random.random(z[i]) * 0.01)
+        return samples
+
+
+class Gaussian(object):
+    """Multivariate Gaussian Distribution
+    """
+
+    def __init__(self, mu=(0.0, 0.0), sigma=(1.0, 1.0)):
+        self.mu = mu
+        self.sigma = sigma
+        self.cov = np.diag(sigma)
+
+    def sample(self, num_samples):
+        return np.random.multivariate_normal(mean=self.mu, cov=self.cov, size=num_samples)
+
+    def logpdf(self, samples):
+        return np.mean(np.log(multivariate_normal.pdf(samples, mean=self.mu, cov=self.sigma)
+                              + epsilon()))
