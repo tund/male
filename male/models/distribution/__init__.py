60027b263b5576f4290b6de4cc0b4c78cd539be7
Tu Dinh Nguyen <nguyendinhtu@gmail.com>
2018-11-10 09:23:37 +1100

    Clean up

--- male/models/distribution/__init__.py
+++
@@ -1,7 +0,0 @@
-from .mixture import GMM
-from .mixture import GMM1D
-from .uniform import Uniform
-from .uniform import Uniform1D
-from .gaussian import Gaussian
-from .gaussian import Gaussian1D
-from .gaussian import InverseGaussian1D
